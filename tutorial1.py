fce65c496a762c6c0ed08dd73c8dde1b92b5c77d
Trenton Creamer <trentoncreamer@Trentons-MBP.home>
2022-10-22 22:01:32 -0700

    Web page opening and searching

--- tutorial1.py
+++ tutorial1.py
@@ -8,11 +8,31 @@
 from selenium import webdriver
 # Fixing the deprecrated warning
 from selenium.webdriver.chrome.service import Service
+# Access to things like the enter and escape keys 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 
 
 s = Service("/usr/local/bin/chromedriver")
 # Loading the driver to the browser that we want to use 
 driver =  webdriver.Chrome(service=s)
 
-driver.get("https://opensea.io/collection/decentraland")
+#Opening the browser
+driver.get("https://www.techwithtim.net/")
+# driver.get("https://opensea.io/collection/decentraland")
 
+print(driver.title)
+
+# ID -> Name - > Class priority or by value
+# Finding the search field
+search = driver.find_element(By.NAME, "s")
+# Entering test
+search.send_keys("Trenton is cool")
+# Hitting enter to return the results 
+search.send_keys(Keys.RETURN)
+
+#driver.quit()
+# <input class="gLFyf gsfi" jsaction="paste:puy29d;" maxlength="2048" name="q" type="text" aria-autocomplete="both"
+#  aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" autofocus="" role="combobox" 
+#  spellcheck="false" title="Search" value="" aria-label="Search" data-ved="0ahUKEwjIo_nKx_X6AhVkHTQIHet2Bh0Q39UDCAY">
