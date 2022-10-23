af7dd3b3188c5556cba86d32c3116c067528624d
Trenton Creamer <trentoncreamer@Trentons-MBP.home>
2022-10-23 13:19:30 -0700

    Storing articles from main and then looping through all of the titles

--- tutorial1.py
+++ tutorial1.py
@@ -5,13 +5,15 @@
 # Step 2: download Chrome Driver
 
 # The webdriver is driving all of our interactions with the browser 
+
 from selenium import webdriver
 # Fixing the deprecrated warning
 from selenium.webdriver.chrome.service import Service
 # Access to things like the enter and escape keys 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 s = Service("/usr/local/bin/chromedriver")
@@ -28,11 +30,31 @@ print(driver.title)
 # Finding the search field
 search = driver.find_element(By.NAME, "s")
 # Entering test
-search.send_keys("Trenton is cool")
+search.send_keys("test")
 # Hitting enter to return the results 
 search.send_keys(Keys.RETURN)
 
+# waiting before we move on to the next part of our script
+try:
+    # waiting a max of 10 seconds
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    print("test")
+    # Find all of the articles
+    # print(main.text)
+    articles =  main.find_elements(by = By.TAG_NAME, value = "article")
+    print(articles)
+    for article in articles:
+        header = article.find_element(by=By.CLASS_NAME,value="entry-summary")
+        print(header.text)
+    # Only quit if it doesn't load properly
+finally:
+    driver.quit()
+
+
+
+# Scraping the full website  
+#print(driver.page_source)
+
 #driver.quit()
-# <input class="gLFyf gsfi" jsaction="paste:puy29d;" maxlength="2048" name="q" type="text" aria-autocomplete="both"
-#  aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" autofocus="" role="combobox" 
-#  spellcheck="false" title="Search" value="" aria-label="Search" data-ved="0ahUKEwjIo_nKx_X6AhVkHTQIHet2Bh0Q39UDCAY">
